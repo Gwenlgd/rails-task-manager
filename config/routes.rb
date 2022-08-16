f3810881cd2a7d452c360fea46d745c36ef1f351
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2022-08-16 12:15:24 +0800

    add tasks model, controller, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/tasks'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/tasks", to: "tasks#index"
+  get "/tasks/:id", to: "tasks#show", as: :task
+  get "tasks/new", to: "tasks#new"
+  post "/tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  delete "laons/:id", to: "tasks#destroy", as: :delete_task
 end
